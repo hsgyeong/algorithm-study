696374ba4ae7e75f25b074d88e85d5f093493416
hsgyeong <115066507+hsgyeong@users.noreply.github.com>
2024-01-30 22:04:00 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int [num_list.length];
+        int n = 0;
+        
+        for(int i=num_list.length-1; i>=0; i--)
+        {
+            if(n<=num_list.length)
+            {   
+                answer[n] = num_list[i];
+                n++;
+            }   
+        }
+        
+        return answer;
+    }
+}
