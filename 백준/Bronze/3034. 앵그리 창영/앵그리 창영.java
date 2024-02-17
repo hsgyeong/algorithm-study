81e92db6de50c69d1dd8fa7192ac645a011feaaf
hsgyeong <115066507+hsgyeong@users.noreply.github.com>
2024-02-17 10:08:55 +0900

    [Bronze III] Title: 앵그리 창영, Time: 212 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/3034. 앵그리 창영/앵그리 창영.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int W = sc.nextInt();
+		int H = sc.nextInt();
+		String answer = "";
+		
+		for(int i=0; i<N; i++)
+		{
+			int length = sc.nextInt();
+			
+			if(length<=W || length<=H || (length*length)<=(W*W)+(H*H))
+			{
+				answer = "DA";
+			}
+			else
+				answer = "NE";
+			
+			System.out.println(answer);
+		}
+			
+	}
+
+}
