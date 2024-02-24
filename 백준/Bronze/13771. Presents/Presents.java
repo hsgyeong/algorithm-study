e882623e24d77a8d4aaad779e743338c12a7e015
hsgyeong <115066507+hsgyeong@users.noreply.github.com>
2024-02-24 16:10:52 +0900

    [Bronze II] Title: Presents, Time: 244 ms, Memory: 18408 KB -BaekjoonHub

---
+++ 백준/Bronze/13771. Presents/Presents.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		double arr[] = new double[T];
+		
+		for(int i=0; i<T; i++)
+		{
+			arr[i] = sc.nextDouble(); 
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.printf("%.2f",arr[1]);
+	}
+
+}
