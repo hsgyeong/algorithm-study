e79e8da0536014bc0d9d5fac1ebe56066f3e283c
hsgyeong <115066507+hsgyeong@users.noreply.github.com>
2024-03-09 18:09:55 +0900

    [Silver V] Title: 칠무해, Time: 2356 ms, Memory: 385220 KB -BaekjoonHub

---
+++ 백준/Silver/14729. 칠무해/칠무해.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		double score[] = new double[N];
+		double arr[] = new double[7];
+		
+		for(int i=0; i<N; i++)
+		{
+			st = new StringTokenizer(br.readLine());
+			
+			score[i] = Double.parseDouble(st.nextToken());
+		}
+		
+		Arrays.sort(score);
+		
+		int idx = 0;
+		
+		for(int j=0; j<7; j++)
+		{
+			arr[idx++] = score[j];
+		}
+		
+		for(double answer : arr)
+		{
+			bw.write(String.format("%.3f",answer)+"\n");
+		}
+			bw.flush();
+			bw.close();
+			br.close();
+	}
+}
