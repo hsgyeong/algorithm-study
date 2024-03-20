b5f42845f70ed650b2ac40965dca007513b6bd8b
hsgyeong <115066507+hsgyeong@users.noreply.github.com>
2024-03-20 11:54:18 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 360 ms, Memory: 23720 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,204 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String word = sc.next();
+		int a = -1;
+		int b = -1;
+		int c = -1;
+		int d = -1;
+		int e = -1;
+		int f = -1;
+		int g = -1;
+		int h = -1;
+		int i = -1;
+		int j = -1;
+		int k = -1;
+		int l = -1;
+		int m = -1;
+		int n = -1;
+		int o = -1;
+		int p = -1;
+		int q = -1;
+		int r = -1;
+		int s = -1;
+		int t = -1;
+		int u = -1;
+		int v = -1;
+		int w = -1;
+		int x = -1;
+		int y = -1;
+		int z = -1;
+		
+		for(int ii=0; ii<word.length(); ii++)
+		{
+				if(word.substring(ii,ii+1).equals("a"))
+				{
+					if(a==-1)
+					{
+						a = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("b"))
+				{
+					if(b==-1)
+					{
+						b = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("c"))
+				{
+					if(c==-1)
+					{
+						c = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("d"))
+				{
+					if(d==-1)
+					{
+						d = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("e"))
+				{
+					if(e==-1)
+					{
+						e = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("f"))
+				{
+					if(f==-1)
+					{
+						f = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("g"))
+				{
+					if(g==-1)
+					{
+						g = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("h"))
+				{
+					if(h==-1)
+					{
+						h = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("i"))
+				{	
+					if(i==-1)
+					{
+						i = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("j"))
+				{
+					if(j==-1)
+					{
+						j = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("k"))
+				{
+					if(k==-1)
+					{
+						k = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("l"))
+				{
+					if(l==-1)
+					{
+						l = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("m"))
+				{
+					if(m==-1)
+					{
+						m = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("n"))
+				{
+					if(n==-1)
+					{
+						n = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("o"))
+				{
+					if(o==-1)
+					{
+						o = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("p"))
+				{
+					if(p==-1)
+					{
+						p = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("q"))
+				{
+					if(q==-1)
+					{
+						q = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("r"))
+				{
+					if(r==-1)
+					{
+						r = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("s"))
+				{
+					if(s==-1)
+					{
+						s = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("t"))
+				{
+					if(t==-1)
+					{
+						t = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("u"))
+				{
+					if(u==-1)
+					{
+						u = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("v"))
+				{
+					if(v==-1)
+					{
+						v = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("w"))
+				{
+					if(w==-1)
+					{
+						w = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("x"))
+				{
+					if(x==-1)
+					{
+						x = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("y"))
+				{
+					if(y==-1)
+					{
+						y = ii;
+					}
+				}else if(word.substring(ii,ii+1).equals("z"))
+				{
+					if(z==-1)
+					{
+						z = ii;
+					}
+				}
+		}
+		
+		System.out.print(a+" "+b+" "+c+" "+d+" "+e+" "+f+" "+g
+		+" "+h+" "+i+" "+j+" "+k+" "+l+" "+m+" "+n+" "+o+" "+p+" "+q
+		+" "+r+" "+s+" "+t+" "+u+" "+v+" "+w+" "+x+" "+y+" "+z);
+	}
+
+
+	}
